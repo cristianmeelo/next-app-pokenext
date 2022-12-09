07815e5d95a7de384c273e6bae180bebd970eead
Cristian Melo <cristianmeelo@gmail.com>
2022-12-08 21:42:27 -0300

    styles: implemented Navbar css

--- pages/components/Navbar.tsx
+++ pages/components/Navbar.tsx
@@ -2,21 +2,19 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
+import styles from "../../styles/Navbar.module.css";
+
+const pokedexIcon: string = require("../../public/images/pokeball.png").default;
+
 const Navbar = () => {
   return (
-    <nav>
-      <div>
-        <p>
-          <Image
-            src="/public/images/pokeball.png"
-            width={30}
-            height={30}
-            alt="Pokedex"
-          />
-        </p>
+    <nav className={styles.navbar}>
+      <div className={styles.logo}>
+        <Image src={pokedexIcon} width={30} height={30} alt="Pokedex" />
+        <h1>PokeNext</h1>
       </div>
-      <h1>PokeNext</h1>
-      <ul>
+      
+      <ul className={styles.link_items}>
         <li>
           <Link href="/">
             <p>Home</p>
