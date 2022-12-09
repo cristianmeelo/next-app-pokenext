b596d350e596c933c59519e6b2b60230f8d71213
Cristian Melo <cristianmeelo@gmail.com>
2022-12-08 22:39:13 -0300

    feat: implemented fetch with pokeapi

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,47 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
-export default function Home() {
+type Ipokemons = {
+  pokemons: {
+    map(arg0: (pokemon: Ipokemon) => JSX.Element): import("react").ReactNode;
+    id: string;
+    name: string;
+  };
+};
+
+type Ipokemon = {
+    id: string;
+    name: string;
+};
+
+export async function getStaticProps() {
+  const maxPokemons = 251;
+  const api = "https://pokeapi.co/api/v2/pokemon/";
+
+  const res = await fetch(`${api}/?limit=${maxPokemons}`);
+  const data = await res.json();
+
+  //add pokemon index
+  data.results.forEach((item: { id: string }, index: string) => {
+    item.id = index + 1;
+  });
+
+  return {
+    props: {
+      pokemons: data.results,
+    },
+  };
+}
+
+export default function Home({ pokemons }: Ipokemons) {
   return (
-<h1>Pokenext</h1>
-  )
+    <div>
+      <ul>
+        {pokemons.map((pokemon: Ipokemon) => (
+          <li key={pokemon.id}>{pokemon.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
