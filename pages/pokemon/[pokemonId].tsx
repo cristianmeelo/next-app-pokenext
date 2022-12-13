b9d350f127733067a8cf913da2223357d70fe83a
Cristian Melo <cristianmeelo@gmail.com>
2022-12-13 17:11:29 -0300

    feat: searching individual pokemon

---
+++ pages/pokemon/[pokemonId].tsx
@@ -0,0 +1,36 @@
+export const getStaticPaths = async () => {
+  const maxPokemons = 251;
+  const api = "https://pokeapi.co/api/v2/pokemon/";
+
+  const res = await fetch(`${api}/?limit=${maxPokemons}`);
+  const data = await res.json();
+
+  const paths = data.results.map((pokemon: any, index: number) => {
+    return {
+      params: { pokemonId: (index + 1).toString() },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async (context: {
+  params: { pokemonId: any };
+}) => {
+  const id = context.params.pokemonId;
+
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+
+  const data = await res.json();
+
+  return {
+    props: { pokemon: data },
+  };
+};
+
+export default function Pokemon({ pokemon }: any) {
+  return <h2>{pokemon.name}</h2>;
+}
