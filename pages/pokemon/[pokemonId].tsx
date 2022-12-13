767cf60f78c218e2c02c0bcd93e841162fbb4b02
Cristian Melo <cristianmeelo@gmail.com>
2022-12-13 17:48:38 -0300

    styles: styled pokemon details

--- pages/pokemon/[pokemonId].tsx
+++ pages/pokemon/[pokemonId].tsx
@@ -1,3 +1,14 @@
+import Image from "next/image";
+import {
+  ReactElement,
+  JSXElementConstructor,
+  ReactFragment,
+  ReactPortal,
+  Key,
+} from "react";
+
+import styles from "../../styles/Pokemon.module.css";
+
 export const getStaticPaths = async () => {
   const maxPokemons = 251;
   const api = "https://pokeapi.co/api/v2/pokemon/";
@@ -32,5 +43,53 @@ export const getStaticProps = async (context: {
 };
 
 export default function Pokemon({ pokemon }: any) {
-  return <h2>{pokemon.name}</h2>;
+  return (
+    <div className={styles.pokemon_container}>
+      <h1 className={styles.title}>{pokemon.name}</h1>
+      <Image
+        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`}
+        width={120}
+        height={120}
+        alt={pokemon.name}
+      />
+      <div>
+        <h3>Número:</h3>
+        <p>#{pokemon.id}</p>
+      </div>
+
+      <div>
+        <h3>Tipo:</h3>
+        <div className={styles.types_container}>
+          {pokemon.types.map(
+            (
+              item: {
+                type: {
+                  name: string;
+                };
+              },
+              index: Key
+            ) => (
+              <span
+                key={index}
+                className={`${styles.type} ${styles['type_' + item.type.name]}`}>
+                {item.type.name}
+              </span>
+            )
+          )}
+        </div>
+      </div>
+
+      <div className={styles.data_container}>
+        <div className={styles.height}>
+          <h4>Altura:</h4>
+          <p>{pokemon.height * 10} cm</p>
+        </div>
+
+        <div className={styles.data_height}>
+          <h4>Peso:</h4>
+          <p>{pokemon.weight / 10} kg</p>
+        </div>
+      </div>
+    </div>
+  );
 }
